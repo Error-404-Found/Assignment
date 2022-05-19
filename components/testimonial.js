e6c969351db5edaad145a5209c424d1d32442afb
Gaurav Kadnurrao <gaurav.kadnurrao@schbang.com>
2022-05-19 17:03:48 +0530

    fix: added new testimonials

--- components/testimonial.js
+++ components/testimonial.js
@@ -49,26 +49,54 @@ export default function Testimonials() {
                             <div className={styles.testimonial_container}>
                                 <div className='row'>
                                     <div className='col-lg-4 col-md-4 col-12 d-block d-md-none d-lg-none'>
-                                        <Image src='/images/adam.png' alt='Author' width={289} height={351} />
+                                        <Image src='/images/sam.png' alt='Author' width={289} height={351} />
                                     </div>
                                     <div className='col-lg-8 col-md-8 col-12'>
                                         <div className={styles.testimonial}>
                                             <Image src='/images/Vector.png' width={57} height={45} alt='Quote' className={styles.quote} />
-                                            <h3 className={styles.testimonial_title}>Klenty has helped us engage a far greater number of leads per week, resulting in an outbound revenue jump of 93%</h3>
+                                            <h3 className={styles.testimonial_title}>From someone who is relatively new to sales and lead generating, Klenty was a highly effective and simple-to-use tool that allowed me to quickly generate leads in a genuine and customisable manner</h3>
                                             <div className={styles.authors_info}>
                                                 <div className={styles.logo}>
-                                                    <Image src='/images/double-logo.png' alt='' width={88} height={48} />
+                                                    <Image src='/images/jump-logo.png' alt='' width={88} height={48} />
                                                 </div>
                                                 <Image src='/images/Line.png' alt='' width={1} height={48} />
                                                 <span className={styles.author_container}>
-                                                    <h4 className={styles.author}>Adam Weinger</h4>
-                                                    <p className={styles.author_designation}>President,Double The Donation</p>
+                                                    <h4 className={styles.author}>Sam W </h4>
+                                                    <p className={styles.author_designation}>Head of Demand Generation , Jump </p>
                                                 </span>
                                             </div>
                                         </div>
                                     </div>
                                     <div className='col-lg-4 col-md-4 col-12 d-none d-md-block d-lg-block'>
-                                        <Image src='/images/adam.png' alt='Author' width={289} height={351} />
+                                        <Image src='/images/sam.png' alt='Author' width={289} height={351} />
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.testimonial_container}>
+                                <div className='row'>
+                                    <div className='col-lg-4 col-md-4 col-12 d-block d-md-none d-lg-none'>
+                                        <Image src='/images/Group 1008.png' alt='Author' width={289} height={351} />
+                                    </div>
+                                    <div className='col-lg-8 col-md-8 col-12'>
+                                        <div className={styles.testimonial}>
+                                            <Image src='/images/Vector.png' width={57} height={45} alt='Quote' className={styles.quote} />
+                                            <h3 className={styles.testimonial_title}>It changed our business. Klenty allowed our outbounds sales team to leverage their time and skills</h3>
+                                            <div className={styles.authors_info}>
+                                                <div className={styles.logo}>
+                                                    <Image src='/images/red-eye.png' alt='' width={88} height={48} />
+                                                </div>
+                                                <Image src='/images/Line.png' alt='' width={1} height={48} />
+                                                <span className={styles.author_container}>
+                                                    <h4 className={styles.author}>Gavin Tye</h4>
+                                                    <p className={styles.author_designation}>Sales Director , Red Eye</p>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='col-lg-4 col-md-4 col-12 d-none d-md-block d-lg-block'>
+                                        <Image src='/images/Group 1008.png' alt='Author' width={289} height={351} />
                                     </div>
                                 </div>
                             </div>
